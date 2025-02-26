f5e3ea3a6d2e2a4a6cbe3f0fbf14e887550af33c
Rocky9852 <abhisheklkr628@gmail.com>
2025-02-26 19:47:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/saragamaupdate")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Saregama_Music_Support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/Music_World_Update")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Music_World_Support")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
