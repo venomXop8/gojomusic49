86c7d1e58e8f83655f05b7b3466ef6b9803a16e9
HKMUSIC <himanshu4hk9@gmail.com>
2025-05-30 13:13:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/shoaib910385/COLDAPK",
+    "https://github.com/hkmusic/gojomusic49",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
